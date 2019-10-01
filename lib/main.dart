c3a717eb3a1045da1a2167f5a3e57436ddf63929
Rafael Araújo <rafael.gloria@crptecnologia.com.br>
2019-10-01 17:42:52 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'app/views/pages/main/MainPage.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
