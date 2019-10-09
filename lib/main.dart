232c5c5e898824d89551e4a993c1c7ebf3d4c771
Rafael Araújo <rafael.gloria@crptecnologia.com.br>
2019-10-09 14:28:10 -0300

    - Adicionado código de background

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,91 @@
+
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_location/flutter_background_location.dart';
 import 'package:livebus/app/core/values/colors.dart';
 import 'package:livebus/app/views/pages/home/home.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main(){
+Database _database;
+String loca;
+
+void main()async{
+  Database dt = await database();
+  print("abriu!");
+  FlutterBackgroundLocation.startLocationService();
+  FlutterBackgroundLocation.getLocationUpdates((location) {
+    newPos(location);
+  });
   runApp(MaterialApp(
     title: "Live Bus - App",
     theme: ThemeData(hintColor: primaryColor, primaryColor: lightColor),
-    home: Home(),
+    home: Container(
+      child: DataB()
+    ),
   ));
+}
+
+class DataB extends StatefulWidget {
+  @override
+  _DataBState createState() => _DataBState();
+}
+
+class _DataBState extends State<DataB> {
+  String texto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+            "saved: "
+        )
+      ],
+    );
+  }
+}
+
+void setTexto(String texto){
+
+}
+
+Future<Database> database() async {
+  if (_database != null)
+    return _database;
+
+  // if _database is null we instantiate it
+  _database = await initDB();
+  return _database;
+}
+
+initDB() async {
+  Directory documentsDirectory = await getApplicationDocumentsDirectory();
+  print(documentsDirectory.path);
+  String path =  join(documentsDirectory.path, "livebdb.db");
+  return await openDatabase(path, version: 1, onOpen: (db) {
+  }, onCreate: (Database db, int version) async {
+    await db.execute("CREATE TABLE positions ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "latitute TEXT,"
+        "longitude TEXT"
+        ")");
+  });
+}
+
+newPos(location) async {
+  final db = await database();
+  var res = await db.rawInsert(
+      "INSERT Into positions (latitute, longitude)"
+          " VALUES ("+ location.latitude.toString() +", "+ location.longitude.toString() +")");
+  loca = location.latitude.toString() +", "+ location.longitude.toString();
+  print("inseriu! " + loca);
+  return res;
+}
+
+class DBProvider {
+  DBProvider._();
+  static final DBProvider db = DBProvider._();
 }
